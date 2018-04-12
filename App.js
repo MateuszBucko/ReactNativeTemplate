bafa67e684a07f8aec14e44c9a19ff9f3c24e38f
mbucko <mateuszbucko1@gmail.com>
2018-04-12 19:41:20 +0200

    .

--- App.js
+++ App.js
@@ -1,23 +1,72 @@
-import React, {Component} from 'react';
-import {
-    Text,
-} from 'react-native';
+import React from 'react';
+import {ListView, ActivityIndicator, Text, View, TouchableOpacity} from 'react-native';
 
+var url = 'https://newsapi.org/v2/top-headlines?' +
+    'country=us&' +
+    'apiKey=1b2f331e47184791a519da44a80a49c7';
 
-export default class App extends Component<Props> {
+export default class App extends React.Component {
+
     constructor(props) {
         super(props);
+        this.state = {isLoading: true}
+    }
 
+    componentDidMount() {
+        return fetch(url)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+                this.setState({
+                    isLoading: false,
+                    dataSource: ds.cloneWithRows(responseJson.articles),
+                }, function () {
+                });
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    ListViewItemSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: .5,
+                    width: "100%",
+                    backgroundColor: "#000",
+
+                }}
+            />
+        );
     }
 
 
     render() {
-        return (
-            <Text>Test</Text>
 
+        if (this.state.isLoading) {
+            return (
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
+
+        return (
+            <View>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderSeparator={this.ListViewItemSeparator}
+                    renderRow={(rowData) =>
+                        <View>
+                            <TouchableOpacity/>
+                            <Text>{rowData.title}</Text>
+                        </View>
+                    }
+                />
+
+            </View>
         );
     }
-
 }
-
-
