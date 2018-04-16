d69027762d28ca6a5841dc81392b61f3cb88d575
mbucko <mateuszbucko1@gmail.com>
2018-04-16 12:49:32 +0200

    cd

--- App.js
+++ App.js
@@ -6,8 +6,19 @@ import MainList from './Screens/MainList';
 import Details from './Screens/Details';
 
 const Navigation = StackNavigator({
-    Home: {screen: MainList},
-    DetailsView: {screen: Details}
+    Home: {
+        screen: MainList, navigationOptions: ({navigation}) => ({
+            title: `Lista wiadomości`,
+            headerStyle: {backgroundColor: 'yellow'},
+            headerTitleStyle: {textAlign: 'center'},
+        }),
+
+    },
+    DetailsView: {
+        screen: Details, navigationOptions: ({navigation}) => ({
+            title: `${navigation.state.params.article.title}`,
+        }),
+    }
 });
 
 export default Navigation;
