75848c18eabc14209c5afe71eecac7a9d8247317
mbucko <mateuszbucko1@gmail.com>
2018-05-14 21:26:04 +0200

    .

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
 import React, {Component} from 'react';
 import {
-    Text,
+    View,
+    Button
 } from 'react-native';
+var Sound = require('react-native-sound');
+var song = null;
 
-
+import Songs from 'android/app/src/main/res/raw';
 export default class App extends Component<Props> {
     constructor(props) {
         super(props);
 
+    console.warn(Songs);
+
+    }
+
+    componentWillMount() {
+        song = new Sound('test.mp3', Sound.MAIN_BUNDLE, (error) => {
+            if (error) {
+                console.warn('failed to load the sound', error);
+                return;
+            }
+            // loaded successfully
+            console.warn('duration in seconds: ' + song.getDuration() + 'number of channels: ' + song.getNumberOfChannels());
+        });
+    }
+
+    play() {
+        if (song != null) {
+            song.play((success) => {
+                if (success) {
+                    console.warn('successfully finished playing');
+                } else {
+                    console.warn('playback failed due to audio decoding errors');
+                    // reset the player to its uninitialized state (android only)
+                    // this is the only option to recover after an error occured and use the player again
+                    song.reset();
+                }
+            });
+        }
+    }
+
+    pause() {
+        song.pause();
+    }
+
+    stop() {
+        song.stop();
     }
 
 
     render() {
         return (
-            <Text>Test</Text>
+            <View>
+                <Button
+                    title="Play"
+                    onPress={this.play.bind(this)}/>
+                <Button
+                    title="Pause"
+                    onPress={this.pause.bind(this)}/>
+
+                <Button
+                    title="Stop"
+                    onPress={this.stop.bind(this)}/>
+
+            </View>
 
         );
     }
--- android/app/src/main/java/com/reactnativetemplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativetemplate/MainApplication.java
@@ -3,14 +3,11 @@ package com.reactnativetemplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.sensormanager.SensorManagerPackage;
-import com.sensors.RNSensorsPackage;
-import com.reactlibrary.RNSimpleCompassPackage;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.sensormanager.SensorManagerPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -27,9 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SensorManagerPackage(),
-            new RNSensorsPackage(),
-            new RNSimpleCompassPackage()
+            new RNSoundPackage()
       );
     }
 
