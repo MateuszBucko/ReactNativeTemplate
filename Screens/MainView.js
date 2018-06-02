36707f4f60775734326decf735acbcd6dd03dab8
= <=>
2018-06-02 02:19:26 +0200

    cd

--- Screens/MainView.js
+++ Screens/MainView.js
@@ -3,8 +3,14 @@ import {
     Text,
     View,
     AsyncStorage,
-    Button
+    Button,
+    ScrollView,
+    ListView,
+    TouchableOpacity,
+    ActivityIndicator
 } from 'react-native';
+
+const URL = "http://192.168.1.8:8085/"
 
 
 export default class MainView extends Component<Props> {
@@ -13,56 +19,118 @@ export default class MainView extends Component<Props> {
         try {
             return await AsyncStorage.setItem(key, JSON.stringify(value));
         } catch (error) {
-            // console.error('AsyncStorage#setItem error: ' + error.message);
+            console.error('AsyncStorage#setItem error: ' + error.message);
         }
     }
 
     constructor(props) {
 
+
+        super(props);
+
+        //pobranie kluczy
         var keys = [];
-
-
         AsyncStorage.getAllKeys((err, keys) => {
             AsyncStorage.multiGet(keys, (err, stores) => {
                 stores.map((result, i, store) => {
-                    // get at each store's key/value so you can work with it
-                    let key = store[i][0];
-                    let value = store[i][1];
-
-                    keys[i] = key;
+                    keys[i] = store[i][0];
                 });
 
                 this.setState({
-                    data: keys
+                    favourites: keys
                 });
             });
         });
 
-        super(props);
-
-        this.state = {
-            data: ""
-        };
+        this.state = {isLoading: true}
 
 
     }
 
-    componentDidMount(){
-        // this.setItem('k','test');
+
+    componentDidMount() {
+        fetch(URL + 'allRecipes')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+                this.setState({
+                    isLoading: false,
+                    data: ds.cloneWithRows(responseJson),
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
+
+
+    ListViewItemSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 2,
+                    width: "100%",
+                    backgroundColor: "#000",
+                    marginTop: 5,
+                    marginBottom: 5
+
+                }}
+            />
+        );
+    };
+
+    refresh() {
+
+        fetch(URL + 'allRecipes')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+                this.setState({
+                    isLoading: false,
+                    data: ds.cloneWithRows(responseJson),
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
 
 
     render() {
         const {navigate} = this.props.navigation;
+
+        if (this.state.isLoading) {
+            return (
+                <View style={{flex: 1, padding: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
+
         return (
-            <View>
-                <Text>{this.state.data}</Text>
+            <ScrollView>
+
+                <ListView
+                    dataSource={this.state.data}
+                    renderSeparator={this.ListViewItemSeparator}
+                    renderRow={(rowData) =>
+                        <View>
+                            <TouchableOpacity>
+                                <Text style={{textAlign: 'center', fontWeight: "bold"}}>{rowData.name}</Text>
+
+                            </TouchableOpacity>
+                        </View>
+                    }
+                />
                 <Button
                     title="Dodaj przepis"
                     style={{marginTop: 15}}
                     onPress={() =>
                         navigate('AddRecipeView')}/>
-            </View>
+                <Button title="Odśwież"
+                        onPress={() => this.refresh()}/>
+            </ScrollView>
         );
     }
 
